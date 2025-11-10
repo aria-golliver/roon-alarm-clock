e7511478bbd232d588b7706fa4305d146aa6205e
Aria Golliver <aria.golliver@gmail.com>
2025-11-10 22:29:22 +0000

    list zones

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var RoonApi = require("node-roon-api");
 var RoonApiStatus = require("node-roon-api-status");
+var RoonApiTransport = require("node-roon-api-transport");
 
 var roon = new RoonApi({
     extension_id: 'com.frociaggine.alarm-clock',
@@ -8,12 +9,34 @@ var roon = new RoonApi({
     publisher: 'frociaggine.com',
     email: 'aria@frociaggine.com',
     website: 'https://bsky.app/profile/aria.di.frociaggine.com',
-    log_level: 'all'
+    // log_level: 'all',
+
+
+    core_paired: function (core) {
+        let transport = core.services.RoonApiTransport;
+        transport.subscribe_zones(function (cmd, data) {
+            console.log(core.core_id,
+                core.display_name,
+                core.display_version,
+                "-",
+                cmd,
+                JSON.stringify(data, null, '  '));
+        });
+    },
+
+    core_unpaired: function (core) {
+        console.log(core.core_id,
+            core.display_name,
+            core.display_version,
+            "-",
+            "LOST");
+    }
 });
 
 var svc_status = new RoonApiStatus(roon);
 
 roon.init_services({
+    required_services: [RoonApiTransport],
     provided_services: [svc_status]
 });
 
