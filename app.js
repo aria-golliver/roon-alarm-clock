35c754dba930d3b516997e7be49b1e4827964135
Aria Golliver <aria.golliver@gmail.com>
2025-11-10 22:55:35 +0000

    create simple app that can turn on a roon zone, set the volume, and start playback

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ var RoonApi = require("node-roon-api");
 var RoonApiStatus = require("node-roon-api-status");
 var RoonApiTransport = require("node-roon-api-transport");
 
+AlarmZones = {
+    KefQ150: {
+        Id: '160159f398ff576aff46bb2dedfdff98f359',
+        OutputId: '170159f398ff576aff46bb2dedfdff98f359',
+    },
+    AriaEvoX: {
+        Id: '1601f42f1178d925f9f368be5fb3ebb294e1',
+        OutputId: '1701f42f1178d925f9f368be5fb3ebb294e1',
+    },
+}
+
+var AlarmZone = AlarmZones.KefQ150
+var AlarmVolumeRange = [0, 5] // alarm start volume, alarm end volume
+var transport;
 var roon = new RoonApi({
     extension_id: 'com.frociaggine.alarm-clock',
     display_name: "Alarm Clock",
@@ -9,27 +23,25 @@ var roon = new RoonApi({
     publisher: 'frociaggine.com',
     email: 'aria@frociaggine.com',
     website: 'https://bsky.app/profile/aria.di.frociaggine.com',
-    // log_level: 'all',
-
 
     core_paired: function (core) {
-        let transport = core.services.RoonApiTransport;
+        transport = core.services.RoonApiTransport;
         transport.subscribe_zones(function (cmd, data) {
-            console.log(core.core_id,
-                core.display_name,
-                core.display_version,
-                "-",
-                cmd,
-                JSON.stringify(data, null, '  '));
+            // console.log(core.core_id,
+            //     core.display_name,
+            //     core.display_version,
+            //     "-",
+            //     cmd,
+            //     JSON.stringify(data, null, '  '));
         });
     },
 
     core_unpaired: function (core) {
-        console.log(core.core_id,
-            core.display_name,
-            core.display_version,
-            "-",
-            "LOST");
+        // console.log(core.core_id,
+        //     core.display_name,
+        //     core.display_version,
+        //     "-",
+        //     "LOST");
     }
 });
 
@@ -43,3 +55,20 @@ roon.init_services({
 svc_status.set_status("👍 all good 👍", false);
 
 roon.start_discovery();
+
+function reset_volume() {
+    console.log("resetting volume to", AlarmVolumeRange)
+    transport.change_volume(AlarmZone.OutputId, 'absolute', AlarmVolumeRange[1], start_alarm)
+}
+
+function start_alarm() {
+    console.log("starting alarm")
+    transport.control(AlarmZone.Id, 'play')
+}
+
+function enable_radio() {
+    console.log("enabling radio")
+    transport.change_settings(AlarmZone.Id, { auto_radio: true }, reset_volume)
+}
+
+setTimeout(enable_radio, 2000)
