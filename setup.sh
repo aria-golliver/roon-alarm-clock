ef91c7e7ab2e5bf53b522cf0ab258cf109c72585
Aria Golliver <aria.golliver@gmail.com>
2025-11-16 21:42:46 -0800

    quote bettah

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ NODE_VERSION="${NODE_VERSION:-24}"
 SET_TIMEZONE="${SET_TIMEZONE:-America/Los_Angeles}"
 
 if yes_or_no "install nvm v${NVM_VERSION}?"; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh | bash
+    curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v${NVM_VERSION}/install.sh" | bash
     . "$HOME/.nvm/nvm.sh"
 fi
 
@@ -26,12 +26,12 @@ if yes_or_no "install node v${NODE_VERSION}?"; then
     nvm install 24
 fi
 
-echo "current system time:"
+echo 'current system time:'
 timedatectl
-echo ""
+echo ''
 
 if yes_or_no "set timezone to ${SET_TIMEZONE}"; then
-    sudo timedatectl set-timezone  ${SET_TIMEZONE}
+    sudo timedatectl set-timezone "${SET_TIMEZONE}"
 fi
 
 node -v
@@ -40,17 +40,17 @@ npm -v
 cd "$SCRIPT_DIR"
 npm install
 
-echo ""
-echo ""
-echo ""
-echo "success!"
-echo ""
-echo ""
-echo ""
-echo "please add the following line to your crontab, to eg, wake up at 7:30 am every MON-FRI"
-echo "    (run 'crontab -e' to edit)"
-echo ""
-echo "SHELL=/bin/bash"
+echo ''
+echo ''
+echo ''
+echo 'success!'
+echo ''
+echo ''
+echo ''
+echo 'please add the following line to your crontab, to eg, wake up at 7:30 am every MON-FRI'
+echo '    (run 'crontab -e' to edit)'
+echo ''
+echo 'SHELL=/bin/bash'
 echo '30 7 * * MON-FRI (. "/home/roon/.nvm/nvm.sh"; cd "/home/roon/roon-alarm-clock"; WAKE_ME_UP=1 node .) 2>&1 | logger -t roon-alarm-clock'
 echo ""
 echo "and have to good morning :)"
